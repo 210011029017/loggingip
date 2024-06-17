9c349940567f06908d9fef692aca6430d1f4d7be
Sandeep45516 <sandeep@letslocalise.co.uk>
2024-06-17 17:25:57 +0530

    Add route to view IP log

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.get('/', (req, res) => {
     res.send('Your IP address has been logged.');
 });
 
+// Route to view the log file contents
+app.get('/view-logs', (req, res) => {
+    fs.readFile('ip-log.txt', 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Could not read log file.');
+        }
+        res.type('text/plain').send(data);
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
